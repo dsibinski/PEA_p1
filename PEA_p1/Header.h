a8b8feb412a7051c6a7c52725f191cf66d5ea08a
iks111 <dawidsibinski@gmail.com>
2014-10-12 20:22:19 +0200

    empty project
    Empty VS2012 project uploaded

---
+++ PEA_p1/Header.h
@@ -0,0 +1,14 @@
+
+#ifndef header_h
+#define header_h
+
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+#endif
---
+++ PEA_p1/Source.cpp
@@ -0,0 +1,18 @@
+// PEA, Z. Buchalski
+// projekt 1
+// semestr zimowy 2014/2015
+// Dawid Sibiñski, Bart³omiej Grzegorek
+
+#include "Header.h"
+
+using namespace std;
+
+
+
+int main()
+{
+	
+
+
+	system("PAUSE");
+}
