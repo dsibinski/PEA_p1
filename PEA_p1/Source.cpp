b87acacec7e20e0af17268b24d81db7915e53600
iks111 <dawidsibinski@gmail.com>
2014-10-13 08:38:21 +0200

    console commit test

--- PEA_p1/Source.cpp
+++ PEA_p1/Source.cpp
@@ -1,7 +1,8 @@
 // Projektowanie Efektywnych Algorytmów, Z. Buchalski
 // projekt 1
-// semestr zimowy 2014/2015
 // Dawid Sibiñski, Bart³omiej Grzegorek
+// semestr zimowy 2014/2015
+
 
 #include "Header.h"
 
